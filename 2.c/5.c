22b82b572eda8481ade33d1546e62696963ca924
anushkach0401 <anushkachakraborty0401@gmail.com>
2025-08-09 21:15:20 +0530

    add unique element finder program and MATLAB notes

---
+++ 2.c/5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define SIZE 5
+int main()
+{ 
+   int c=0;
+   int arr[SIZE];
+   for(int i=0;i<SIZE;i++)
+   {
+    printf("Enter %d element\n",(i+1));
+    scanf("%d",&arr[i]);
+   }
+
+   for(int j=0;j<SIZE;j++)
+   {  
+        int a=0;
+
+        for(int k=0;k<SIZE;k++)
+        {
+            if(k!=j) // so that the same element is not chkd.otherwise proper output will not come.
+            {
+                if(arr[j]==arr[k])
+                    {
+                        a++;
+                    }
+            }
+        }
+        
+        if(a==0)
+        {
+            printf("%d is unique ",arr[j]);
+            c++;
+        }
+    }
+    printf("\n Number of unique elements is %d\n",c);
+    return 0;
+   }
