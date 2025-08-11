a288127f3457db644cb475d57907c138ff702983
anushkach0401 <anushkachakraborty0401@gmail.com>
2025-08-11 22:46:39 +0530

    add: implement program to check if a string is a palindrome

---
+++ wrdpallin.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int i;
+    int a=0;
+    char str[30];
+    printf("Enter a string\n");
+    gets(str);
+    int l=strlen(str);
+    for(i=0;i<l/2;i++)
+    {
+        if(str[i]==str[l-i-1])
+        a++;
+    }
+    if(a!=0)
+    printf("Pallindromme");
+    else
+    printf("Not pallindromme");
+    return 0;
+}
