8481a76281896af2da97d2b329dc1f0cf1d3c2dc
anushkach0401 <anushkachakraborty0401@gmail.com>
2025-08-21 00:14:12 +0530

    count even num in a seq

---
+++ seqnum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int countnum()
+{
+   int n;
+   printf("Enter num,to stop enter -1\n");
+   scanf("%d",&n);
+   if(n==-1)
+   return 0;
+   if((n!=-1)&&(n%2==0))
+   return 1+countnum();
+   return countnum();
+}
+
+int main()
+{
+   
+    int cnt=0;
+    cnt=countnum();
+    printf("%d",cnt);
+    return 0;
+}
+
+
