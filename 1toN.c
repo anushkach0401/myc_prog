6a7ddb9c367435341a64fa42e417f426de91e91a
anushkach0401 <anushkachakraborty0401@gmail.com>
2025-08-21 12:23:32 +0530

    print 1 to n and opp

--- 1toN.c
+++ 1toN.c
@@ -1,9 +1,20 @@
-#include <stdio.h>
-void printing(int n)
+#include <stdio.h>//very imp programme
+void printnto1(int n)
 {   
     if(n>0)
-    printf("%d",n);  
-    printing(--n);
+   {
+     printf("%d",n);  //print a num
+    printnto1(--n);  //call with next lower num
+   }
+}
+void print1ton(int x)
+{   
+    if(x>1)//prevent printing 0
+   {
+     print1ton(--x);//call with lower fn
+    printf("%d",x);// print 1 first and then keep progressing
+   }
+
 }
 
 int main()
@@ -11,6 +22,8 @@ int main()
     int a;
     printf("number of terms are\n");
     scanf("%d",&a);
-    printing(a);
+    printnto1(a);
+    printf("\n");
+    print1ton(a);
     return 0;
 }
